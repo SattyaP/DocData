27c499ae579401110fe815fda2572f1c25827bd5
SattyaP <stylearning7@gmail.com>
2024-06-19 23:08:23 +0700

    feat(ui): adding authPanel and setup db config for integration with sql

---
+++ src/ta_alpro2/Config.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ta_alpro2;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author 3330
+ */
+public class Config {
+    private static Connection mysqlconfig;
+    public static Connection configDB()throws SQLException {
+        try {
+            String url = "jdbc:mysql://localhost/docdata";
+            String user = "root";
+            String pass = "";
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            mysqlconfig = DriverManager.getConnection(url, user, pass);
+            System.out.println("Koneksi Sukses");
+        } catch (Exception e) {
+            System.out.println("Koneksi gagal " + e.getMessage());
+        }
+        
+        return mysqlconfig;
+    }
+}
